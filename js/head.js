d08b89dba40ff1067a1ffa702b7ba522bc258678
18538527335 <1498952582@qq.com>
2021-05-12 15:00:31 +0800

    Revert "提交丝芙兰"
    This reverts commit 8e499995fc9d6b237e7b08366c0700d00e8276e2.

--- js/head.js
+++
@@ -1,6 +0,0 @@
-//常见问题单行滚动js效果。。。。。。。。。。。。。。。。。。。。。。。。。。
-setInterval(function(){
-    $(".con-t").animate({"margin-top":-30},400,function(){
-        $(".con-t li:first-child").appendTo(".con-t").parent().css("margin-top",0);
-    })
-},2000)
